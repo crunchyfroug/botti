4f4f11e8a45b94b54198daafa5b4057d37b7f5ac
Koskinen Kim <k.j.koskinen@live.fi>
2020-03-30 14:24:03 +0300

    commit 1

---
+++ BotApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Web;
+using System.Text.RegularExpressions;
+
+namespace BotApp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string data;
+            string urlAddress = "https://www.lounaat.info";
+            using (WebClient client = new WebClient())
+                {
+                client.UseDefaultCredentials = true;
+                client.Headers.Add("User-Agent: Other");
+                data = client.DownloadString(urlAddress);
+                    }
+            string pattern = "<div.*?>(.*?)<\\/div>";
+            MatchCollection matches = Regex.Matches(data, pattern);
+            Console.WriteLine("Matches found: {0}", matches.Count);
+            if (matches.Count > 0)
+                {
+                foreach (Match m in matches)
+                Console.WriteLine("{0}", m.Groups[1]);
+                
+            Console.ReadLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
